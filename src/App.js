017c2d54c19704e0105d0aa28866707198184fb4
Gowtham-karthik <21131a05f9@gvpce.ac.in>
2025-06-17 18:56:51 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [item,setItem]=useState();
+  const [selecteditem,setSelectedItem]=useState([]);
+
+  const onAddtodo=(e)=>{
+    setItem(e.target.value);
+  }
+  const onEnterClick=()=>{
+    setSelectedItem([...selecteditem, {id: Date.now(), value: item, checked: false}]);
+  }
+
+  const onDeleteClick = (idToDelete) => {
+    setSelectedItem(selecteditem.filter(item => item.id !== idToDelete));
+  };
+
+  const onToggleCheck = (idToToggle) => {
+    setSelectedItem(
+      selecteditem.map(item =>
+        item.id === idToToggle ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='Heading'>WISHLIST APP</h1>
+      <div>
+        <input onChange={onAddtodo} placeholder='add to do'></input>
+        <button onClick={onEnterClick}>ENTER</button>
+        <ul>
+          {selecteditem.map((todo) => (
+            <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.checked}
+              onChange={() => onToggleCheck(todo.id)}
+            />
+            <span style={{ textDecoration: todo.checked ? 'line-through' : 'none' }}>
+              {todo.value}
+            </span>
+            <button onClick={() => onDeleteClick(todo.id)}>DELETE</button>
+          </li>))}
+        </ul>
+
+      </div>
+    </>
+    
   );
 }
 
